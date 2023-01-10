025108d98c157aba860a0e50a5d494b7bde46521
Ismael Fuentes <ismafuentes@hotmail.es>
2023-01-10 17:55:27 +0100

    lifecicle events

--- app/src/main/java/com/example/asteroides/MainActivity.java
+++ app/src/main/java/com/example/asteroides/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
     // Puntuaciones
     _store = new ScoreStore();
   }
@@ -40,12 +41,35 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onStart() {
     super.onStart();
-
     // Configuración
     HandleButtonsRegistration();
-
     // Animations
     HandleAnimations();
+  }
+
+  @Override
+  protected void onResume(){
+    super.onResume();
+  }
+
+  @Override
+  protected void onPause(){
+    super.onPause();
+  }
+
+  @Override
+  protected void onStop(){
+    super.onStop();
+  }
+
+  @Override
+  protected void onRestart(){
+    super.onRestart();
+  }
+
+  @Override
+  protected void onDestroy(){
+    super.onDestroy();
   }
 
   public boolean onCreateOptionsMenu(Menu menu) {
